a70865e36a37cdc56ccd14b554091a3d691c3a58
Manda Kausthubh <kausthubhh.manda@gmail.com>
2025-02-14 19:23:35 +0530

    MK: Removing print statements

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -18,17 +18,17 @@ class NeuralNetworks():
         for l in self.Layers:
             inputs.append(l.forward(inputs[-1]))
 
-        print("Forward pass shapes")
-        for i in inputs:
-            print(i.shape)
-        print("Forward pass shapes, end")
+        # print("Forward pass shapes")
+        # for i in inputs:
+        #     print(i.shape)
+        # print("Forward pass shapes, end")
 
         FinalLoss = lossFunc.forward(inputs[-1], y)
         grad = lossFunc.grad(inputs[-1], y)
-        print(f"Loss: {FinalLoss}")
+        # print(f"Loss: {FinalLoss}")
         # Followed by a backward pass
         for i in range(self.depth-1, -1, -1):
-            print(f"Layer {i}, grad: {grad.shape}, input: {inputs[i-1].shape}")
+            # print(f"Layer {i}, grad: {grad.shape}, input: {inputs[i-1].shape}")
             b, W, grad = self.Layers[i].backProp(inputs[i], grad)
             self.Layers[i].UpdateWeights(lr*W, lr*b)
 
